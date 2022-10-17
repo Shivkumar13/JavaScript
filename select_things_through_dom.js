ade0481326f5b2f12dedca7b01a3398d56e33bbd
Shivkumar Anil Ople <shivkumar.ople97@gmail.com>
2022-10-17 15:29:49 +0530

    Create select_things_through_dom.js

---
+++ select_things_through_dom.js
@@ -0,0 +1,15 @@
+// Single Element selector
+console.log(document.getElementById('name'));
+
+
+console.log(document.querySelector('h1'));
+console.log(document.querySelector('.container'));
+
+
+//Multiple element selector
+console.log(document.querySelectorAll('.container'))
+console.log(document.querySelectorAll('.item'))
+
+console.log(document.getElementsByClassName('item'))
+console.log(document.getElementsByClassName('container'))
+
